bcf3fefe6704e52a088241b5e00213f0df74f69b
8c6t <nkw8620@gmail.com>
2019-09-19 09:48:55 +0900

    1.5 스프링 IoC 컨테이너와 연동

---
+++ ch01/javaservletdemo/src/main/java/com/hachicore/AppConfig.java
@@ -0,0 +1,11 @@
+package com.hachicore;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class AppConfig {
+
+
+}
