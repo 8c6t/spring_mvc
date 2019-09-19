962d01f9b9d8c62c063414a37005e7e4d194604f
8c6t <nkw8620@gmail.com>
2019-09-19 10:27:18 +0900

    1.6 스프링 MVC 연동

--- ch01/javaservletdemo/src/main/java/com/hachicore/WebConfig.java
+++ ch01/javaservletdemo/src/main/java/com/hachicore/WebConfig.java
@@ -2,11 +2,10 @@ package com.hachicore;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
 
 @Configuration
-@ComponentScan(excludeFilters = @ComponentScan.Filter(Controller.class))
-public class AppConfig {
+@ComponentScan
+public class WebConfig {
 
 
 }
