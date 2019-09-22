572ecf241edf5af5dc072bcdf00db47f3f157117
8c6t <nkw8620@gmail.com>
2019-09-22 19:38:41 +0900

    2.2 @EnableWebMvc

--- ch02/javaservletdemo/src/main/java/com/hachicore/WebApplication.java
+++ ch02/javaservletdemo/src/main/java/com/hachicore/WebApplication.java
@@ -13,6 +13,11 @@ public class WebApplication implements WebApplicationInitializer {
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
 
+        // DispatcherServlet이 사용하는 컨텍스트에 서블릿 컨텍스트를
+        // 미설정시 EnableWebMvc에서 불러오는 DelegatingWebMvcConfiguration를 사용할 때
+        // 서블릿 컨텍스트를 종종 참조하게 되는데 이때 제대로 빈 설정이 되지 않음
+        context.setServletContext(servletContext);
+
         context.register(WebConfig.class);
         context.refresh();
 
