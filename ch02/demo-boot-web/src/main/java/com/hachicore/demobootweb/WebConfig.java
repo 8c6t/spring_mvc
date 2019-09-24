902ac3336884aee43127cd2116279e2145eabf2f
8c6t <nkw8620@gmail.com>
2019-09-24 10:23:02 +0900

    2.10 리소스 핸들러

--- ch02/demo-boot-web/src/main/java/com/hachicore/demobootweb/WebConfig.java
+++ ch02/demo-boot-web/src/main/java/com/hachicore/demobootweb/WebConfig.java
@@ -2,6 +2,7 @@ package com.hachicore.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -13,5 +14,15 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new AnotherInterceptor())
             .addPathPatterns("/hi")
             .order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+                // .addResourceLocations("classpath:/mobile/", "file:/User/Haru/files/")
+                // .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+                .resourceChain(true); // 캐시 사용 여부(dev: true / prod: false)
+
     }
 }
