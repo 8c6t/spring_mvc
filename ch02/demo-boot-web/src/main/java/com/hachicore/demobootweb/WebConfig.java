8a40654220135cd610f6383b210138e2132c2473
8c6t <nkw8620@gmail.com>
2019-09-27 15:38:13 +0900

    2.14 기타 WebMvcConfigurer 설정

--- ch02/demo-boot-web/src/main/java/com/hachicore/demobootweb/WebConfig.java
+++ ch02/demo-boot-web/src/main/java/com/hachicore/demobootweb/WebConfig.java
@@ -5,10 +5,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/hi").setViewName("hi");
+    }
 
     @Bean
     public Jaxb2Marshaller jaxb2Marshaller() {
