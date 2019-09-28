102766836c7b938f3c7fabb6f93c639f2990962f
8c6t <nkw8620@gmail.com>
2019-09-28 14:25:57 +0900

    3.4 요청 맵핑하기 3부 미디어 타입

--- ch03/demo-web-mvc/src/main/java/com/hachicore/demowebmvc/SampleController.java
+++ ch03/demo-web-mvc/src/main/java/com/hachicore/demowebmvc/SampleController.java
@@ -1,10 +1,11 @@
 package com.hachicore.demowebmvc;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/hello")
 public class SampleController {
 
     // @GetMapping({ "/hello", "/hi" })
@@ -16,12 +17,14 @@ public class SampleController {
         return "hello hachicore";
     }
 
-/*
-    @RequestMapping("/**")
+    @RequestMapping(
+            value = "/hello",
+            consumes = MediaType.APPLICATION_JSON_UTF8_VALUE,
+            produces = MediaType.TEXT_PLAIN_VALUE
+    )
     @ResponseBody
     public String hello() {
         return "hello";
     }
-*/
 
 }
--- ch03/demo-web-mvc/src/test/java/com/hachicore/demowebmvc/SampleControllerTest.java
+++ ch03/demo-web-mvc/src/test/java/com/hachicore/demowebmvc/SampleControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -40,4 +41,22 @@ public class SampleControllerTest {
         ;
     }
 
+    @Test
+    public void consumeTest() throws Exception {
+        mockMvc.perform(get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+        ;
+    }
+
+    @Test
+    public void producesTest() throws Exception {
+        mockMvc.perform(get("/hello")
+                        .contentType(MediaType.APPLICATION_JSON))
+                        // .accept(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+        ;
+    }
 }
