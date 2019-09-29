d819d5229c78bc73f9bf01ddee0a1e89d7f05206
8c6t <nkw8620@gmail.com>
2019-09-30 00:59:28 +0900

    3.14 핸들러 메소드 6부 @Validated

--- ch03/demo-web-mvc/src/main/java/com/hachicore/demowebmvc/SampleController.java
+++ ch03/demo-web-mvc/src/main/java/com/hachicore/demowebmvc/SampleController.java
@@ -3,19 +3,18 @@ package com.hachicore.demowebmvc;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.validation.Valid;
 
 @Controller
 public class SampleController {
 
     @PostMapping("/events/name/{name}")
     @ResponseBody
-    public Event postEvent(@Valid @ModelAttribute Event event, BindingResult bindingResult) {
+    public Event postEvent(@Validated(Event.ValitdateName.class) @ModelAttribute Event event, BindingResult bindingResult) {
         if(bindingResult.hasErrors()) {
             System.out.println("==================");
             bindingResult.getAllErrors().forEach(e -> {
